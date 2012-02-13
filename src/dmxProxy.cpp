025f7259a6ea96263144c8da9228a4101a02912a
Monty Taylor <mordred@inaugust.com>
2012-02-12 22:55:36 -0800

    Added copyright info.

--- src/dmxProxy.cpp
+++ src/dmxProxy.cpp
@@ -1,11 +1,31 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  avgVideo - outputs average color values of video frames as DMX
+ *  Copyright (C) 2012 Monty Taylor
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "dmxProxy.h"
+
 #include <ola/Callback.h>
 #include <ola/DmxBuffer.h>
 #include <ola/OlaClient.h>
 #include <ola/OlaClientWrapper.h>
 
-#include "dmxProxy.h"
-
-#include <cstdio>
+#include <iostream>
 #include <cerrno>
 
 DmxProxy::DmxProxy() :
@@ -16,7 +36,7 @@ DmxProxy::DmxProxy() :
     static_cast<ola::SimpleClient *>(ola_client);
 
   if (not simple_client->Setup()) {
-    printf("error: %s", strerror(errno));
+    std::cout << "error: " << strerror(errno) << std::endl;
     exit(1);
   }
   client= simple_client->GetClient();
--- src/testApp.h
+++ src/testApp.h
@@ -1,5 +1,24 @@
-#ifndef _TEST_APP
-#define _TEST_APP
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  avgVideo - outputs average color values of video frames as DMX
+ *  Copyright (C) 2012 Monty Taylor
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include <vector>
 #include <ola/DmxBuffer.h>
@@ -44,5 +63,3 @@ public:
   void windowResized(int w, int h);
 
 };
-
-#endif
